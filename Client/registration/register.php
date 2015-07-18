13de842e2a3afa16d61194e981cde9ce74e65e64
Eveyy <shakiraliu0@gmail.com>
2015-07-18 19:07:57 -0400

    working on php

--- Client/registration/register.php
+++ Client/registration/register.php
@@ -1,3 +1,9 @@
 <?php
-    readfile("../register.html");
+$server = 'localhost';
+$user = 'root';
+$pass = '';
+$dbname = 'fpdb';
+$con = mysql_connect($server, $user, $pass) or die("Can't connect");
+mysql_select_db($dbname);
+echo "It worked";
 ?>
