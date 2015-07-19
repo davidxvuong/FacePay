5ee546c5393f8182249f693990ecb3c7dbd7de4f
David Vuong <david.vuong@uwaterloo.ca>
2015-07-19 04:59:11 -0400

    inital import of payment server

---
+++ Server/src/paymentServerPkg/BraintreeWrapper.java
@@ -0,0 +1,42 @@
+package paymentServerPkg;
+import java.math.BigDecimal;
+import com.braintreegateway.*;
+
+public class BraintreeWrapper {
+	String creditCardNumber;
+	String expDate;
+	String cardHolderName;
+	String cvv;
+	BraintreeGateway gateway;
+	final String PUBLIC_KEY = "x9czf5z3m954m4tm";
+	final String PRIVATE_KEY = "7c729ffb7fd4356ddd13a242dde70f95";
+	final String MERCHANT_ID = "wsrzxrc8fht9bgsm";
+	
+	public BraintreeWrapper(String cardNum, String name, String exp, String cvv) {
+		creditCardNumber = cardNum;
+		expDate = exp;
+		cardHolderName = name;
+		this.cvv = cvv;
+		gateway = new BraintreeGateway(Environment.SANDBOX, MERCHANT_ID, PUBLIC_KEY, PRIVATE_KEY);
+	}
+	
+	public String executePayment(double value) {
+		TransactionRequest req = new TransactionRequest().
+				amount(new BigDecimal(value)).creditCard().
+				number(creditCardNumber).
+				cardholderName(cardHolderName).
+				cvv(cvv).
+				expirationDate(expDate).
+				done();
+		
+		Result<Transaction> result = gateway.transaction().sale(req);
+		
+		if (result.isSuccess()) {
+			Transaction txn = result.getTarget();
+			return txn.getId();
+		}
+		else {
+			return "Failed";
+		}
+	}
+}
--- Server/src/paymentServerPkg/Program.java
+++ Server/src/paymentServerPkg/Program.java
@@ -1,10 +1,27 @@
 package paymentServerPkg;
 
+import java.io.*;
+import java.net.*;
+
 public class Program {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello");
+	private static ServerSocket server;
+	private static Socket client;
+	private static WebSocketService instance;
+	private static InputStream input;
+	private static OutputStream output;
+	
+	public static void main(String[] args) throws IOException {
+		System.out.println("Server started. Accepting connections...");
+
+		server = new ServerSocket(2048);
+		while (true) {
+			client = server.accept();
+			input = client.getInputStream();
+			output = client.getOutputStream();
+			instance = new WebSocketService(client, input, output);
+			instance.start();
+		}
 	}
 
 }
