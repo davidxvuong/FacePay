2f30e8d119f759c9fdb893b77d884f12394a47b3
Eveyy <shakiraliu0@gmail.com>
2015-07-18 21:06:55 -0400

    changed ip address

--- Client/registration/register.php
+++ Client/registration/register.php
@@ -1,8 +1,18 @@
 <?php
-$server = '127.0.0.1';
+$server = '172.17.34.250';
 $user = 'root';
 $pass = '';
 $dbname = 'fpdb';
+
+$name = $_GET["name"];
+$email = $_GET["email"];
+$address = $_GET["address"];
+$phone = $_GET["phone"];
+$creditCardNumber = $_GET["card_number"];
+$expDate = $_GET["date"];
+$secureCode = $_GET["code"];
+$uid = $_GET["uid"];
+
 $con = mysql_connect($server, $user, $pass) or die("Can't connect");
 mysql_select_db($dbname);
 echo "It worked";
